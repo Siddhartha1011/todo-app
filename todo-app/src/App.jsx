71289fe029d2a8bc6b30e0c2f6ae484e41098c84
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-01-29 00:57:13 +0530

    Added Header component and input UI

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,7 +2,22 @@
 import './App.css'
 
 function App() {
-
+  const [text, setText] = useState("")
+  
+  return (
+    <div className="app">
+      <Header />
+      <div className="input-box">
+        <input
+          type="text"
+          value={text}
+          placeholder="Add a task..."
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default App
